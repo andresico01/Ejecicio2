97c47d38fcb5698723affac1fad85b6627938e30
rodrigo <>
2021-04-07 20:05:18 +0000

    ss

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ if ( num > 10){
 }
 console.log("Ejercicio 7");
 let cont = 1;
-const aux = Math.floor(random()*10)+1;
+const aux = Math.floor(Math.random()*10)+1
 let valor;
 while(cont){
-  valor = parseInt(prompt("ingrese un numero entre 1 y 10"))
+  valor = parseInt(prompt("ingrese un numero entre 1 y 10"));
   if( valor <= 10 && valor >=1){
     if (valor === aux){
       cont = 0;
@@ -25,3 +25,18 @@ while(cont){
 
 }
 console.log("Ejercicio 8");
+cont = parseInt(prompt("ingrese un numero"));
+let salida = (cont % 5) === 0 ? "Si, el número x es múltiplo 5" : "No, el número x no es múltiplo de 5";
+console.log(salida);
+
+console.log("Ejercicio 9");
+num = parseInt(prompt("Inglese un numero"));
+
+if ( num > 10){
+  console.log("numero mayor que 10");
+}else if(num < 10){
+  console.log("numero menor que 10");
+}else{
+  console.log("es igual a 10");
+}
+console.log("Ejercicio 10");
