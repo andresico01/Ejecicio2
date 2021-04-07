95c035201c9b2c2650dc0a0fa707f9e790cd4c98
rodrigo <>
2021-04-07 19:51:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("Ejercicio 6");
+let num = parseInt(prompt("Inglese un numero"));
+
+if ( num > 10){
+  console.log("numero mayor que 10");
+}else if(num < 10){
+  console.log("numero menor que 10");
+}else{
+  console.log("es igual");
+}
+console.log("Ejercicio 7");
+let cont = 1;
+const aux = Mat.floor(random()*10)+1;
+let valor;
+while(cont){
+  valor = parseInt(prompt("ingrese un numero entre 1 y 10"))
+  if( valor <= 10 && valor >=1){
+    if (valor === aux){
+      cont = 0;
+      console.log("Felicitaciones encontro el numero");
+    }
+  }else{
+    console.log("fuera del rango de 1 a 10 vuelva a introducir un numero",aux);
+  }
+
+}
+console.log("Ejercicio 8");
